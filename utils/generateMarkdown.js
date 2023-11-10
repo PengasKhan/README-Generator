8fcd041a7355ea9d1c51e52405d1a1f57ef70193
PengasKhan <cheesepuff42@gmail.com>
2023-11-09 18:02:33 -0600

    Markdown rough draft complete
    functions filled out

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,64 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
+  switch (license) {
+    case "mit":
+      return "![https://img.shields.io/badge/License-MIT-yellow.svg](https://choosealicense.com/licenses/mit/)";
+    case "gpl-3.0":
+      return "![https://img.shields.io/badge/License-GPL%203.0-blue.svg](https://choosealicense.com/licenses/gpl-3.0/)";
+    case "apache-2.0":
+      return "![https://img.shields.io/badge/License-Apache%202.0-blue.svg](https://choosealicense.com/licenses/apache-2.0/)";
+    case "isc":
+      return "![https://img.shields.io/badge/License-ISC-green.svg](https://choosealicense.com/licenses/isc/)";
+    default:
+      return "";
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "mit":
+      return "[Link to Liscense information](https://choosealicense.com/licenses/mit/)";
+    case "gpl-3.0":
+      return "[Link to Liscense information](https://choosealicense.com/licenses/gpl-3.0/)";
+    case "apache-2.0":
+      return "[Link to Liscense information](https://choosealicense.com/licenses/apache-2.0/)";
+    case "isc":
+      return "[Link to Liscense information](https://choosealicense.com/licenses/isc/)";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case "mit":
+      return "This repo is covered by an MIT license";
+    case "gpl-3.0":
+      return "This repo is covered by a GNU General Public License v3.0";
+    case "apache-2.0":
+      return "This repo is covered by an Apache License 2.0";
+    case "isc":
+      return "This repo is covered by an ISC license";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licenseBadge = renderLicenseBadge(answers.license);
+  const licenseLink = renderLicenseLink(answers.license);
+  const licenseSection = renderLicenseSection(answers.license);
+
+  return `# ${data.title} ${licenseBadge}
 
 ##Description
-${data.title}
+${data.description}
 
 ##Table of Contents
 - [Installation](#installation)
@@ -37,9 +78,10 @@ ${answers.usage}
 ${answers.contribution}
 
 ##Tests
-${answers.tests
-}
+${answers.tests}
 ##License
+${licenseSection}
+${licenseLink}
 
 ##Questions
 [Github Profile: https://github.com/${answers.github}](https://github.com/${answers.github})
